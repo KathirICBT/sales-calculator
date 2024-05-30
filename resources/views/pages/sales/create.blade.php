81efa8ae3f5239242520ef1c1ecef602ca32ded8
KathirICBT <sanjayicbt@gmail.com>
2024-05-31 02:24:20 +0530

    imageinsale

--- resources/views/pages/sales/create.blade.php
+++ resources/views/pages/sales/create.blade.php
@@ -4,7 +4,23 @@
 
     <x-content-header title="Sales" />
     <x-alert-message />
-    
+
+    <div class="row">
+        <div class="col-12 col-md-12 d-flex">
+            <div class="card flex-fill border-0">
+                <div class="card-body p-0 d-flex flex-fill">
+                    <div class="row g-0 w-100">
+                        <div class="p-3 m-1 w-100 d-flex justify-content-end">
+                            <button type="button" class="btn btn-success rounded-pill" data-bs-toggle="modal" data-bs-target="#billInfoModal">
+                                Add Bill Image
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-12 col-md-12 d-flex">
             <div class="card flex-fill border-0">
@@ -25,43 +41,46 @@
                                     @csrf
                                     <div class="col-md-6">
                                         @foreach($staffs as $staff)
-                                        @if(session('username')==$staff->username ||session('adminusername')==$staff->username)
+                                        @if(session('username')==$staff->username
+                                        ||session('adminusername')==$staff->username)
                                         <label for="staff_name" class="form-label">User: </label>
-                                        <input type="text" class="form-control" id="staff_name" name="staff_name" value="{{ $staff->staff_name }}" readonly>
+                                        <input type="text" class="form-control" id="staff_name" name="staff_name"
+                                            value="{{ $staff->staff_name }}" readonly>
                                         <input type="hidden" id="staff_id" name="staff_id" value="{{ $staff->id }}">
                                         @endif
                                         @endforeach
                                     </div>
-                                    
+
                                     @if(session()->has('adminusername'))
-                                        <div class="col-md-6">
-                                            <label for="shop_id" class="form-label">Shop:</label>
-                                            <select name="shop_id" id="shop_id" class="form-select">
-                                                <option value="" >Select a Shop</option>
-                                                @foreach($shops as $shop)
-                                                    <option value="{{ $shop->id }}">{{ $shop->name }}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                    @else
-                                        <div class="col-md-6">
-                                            @foreach($staffs as $staff)
-                                            @if(session('username')==$staff->username)
-                                                <label for="shop_name" class="form-label">Shop: </label>
-                                                @if ($staff->shop && $staff->shop->name)
-                                                    <input type="text" class="form-control" id="shop_name" name="shop_name" value="{{ $staff->shop->name }}" readonly>
-                                                    <input type="hidden" id="shop_id" name="shop_id" value="{{ $staff->shop_id }}">
-                                                @else                                                                                                   
-                                                    <select name="shop_id" id="shop_id" class="form-select">
-                                                        <option value="" >Select a Shop</option>
-                                                        @foreach($shops as $shop)
-                                                            <option value="{{ $shop->id }}">{{ $shop->name }}</option>
-                                                        @endforeach
-                                                    </select>                                                
-                                                @endif                                            
-                                            @endif
+                                    <div class="col-md-6">
+                                        <label for="shop_id" class="form-label">Shop:</label>
+                                        <select name="shop_id" id="shop_id" class="form-select">
+                                            <option value="">Select a Shop</option>
+                                            @foreach($shops as $shop)
+                                            <option value="{{ $shop->id }}">{{ $shop->name }}</option>
                                             @endforeach
-                                        </div>
+                                        </select>
+                                    </div>
+                                    @else
+                                    <div class="col-md-6">
+                                        @foreach($staffs as $staff)
+                                        @if(session('username')==$staff->username)
+                                        <label for="shop_name" class="form-label">Shop: </label>
+                                        @if ($staff->shop && $staff->shop->name)
+                                        <input type="text" class="form-control" id="shop_name" name="shop_name"
+                                            value="{{ $staff->shop->name }}" readonly>
+                                        <input type="hidden" id="shop_id" name="shop_id" value="{{ $staff->shop_id }}">
+                                        @else
+                                        <select name="shop_id" id="shop_id" class="form-select">
+                                            <option value="">Select a Shop</option>
+                                            @foreach($shops as $shop)
+                                            <option value="{{ $shop->id }}">{{ $shop->name }}</option>
+                                            @endforeach
+                                        </select>
+                                        @endif
+                                        @endif
+                                        @endforeach
+                                    </div>
                                     @endif
                                     <div class="col-md-3">
                                         <label for="shift_start_date" class="form-label">Shift Start Date:</label>
@@ -115,7 +134,8 @@
                                         action="{{ route('shifts.store.submit') }}">
                                         @csrf
                                         <table class="table" id="repeater-table">
-                                            <thead style="position: sticky; top: 0; background-color: #1a1d20; z-index: 1;">
+                                            <thead
+                                                style="position: sticky; top: 0; background-color: #1a1d20; z-index: 1;">
                                                 <tr>
                                                     <th>Department</th>
                                                     <th>Amount</th>
@@ -361,7 +381,8 @@
                                     <hr>
 
                                     <button id="submit-btn" class="btn btn-success rounded-pill mt-3"
-                                        style="width: 100%"><i class="fa-solid fa-floppy-disk me-1"></i> Submit </button>
+                                        style="width: 100%"><i class="fa-solid fa-floppy-disk me-1"></i> Submit
+                                    </button>
                                     {{-- <button type="submit-btn" class="btn btn-primary rounded-pill">Submit</button>
                                     --}}
                                 </div>
@@ -376,7 +397,7 @@
 
 
         <!-- Forms -->
-        <div class="row">
+        {{-- <div class="row">
             <div class="col-12 col-md-6 d-flex">
                 <div class="card flex-fill border-0">
                     <div class="card-body p-0 d-flex flex-fill">
@@ -504,12 +525,12 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
         <!-- Forms end -->
     </div>
     @endsection
     <!-- Edit Staff Modal -->
-    <div class="modal fade" id="editShiftModal" tabindex="-1" role="dialog" aria-labelledby="editShiftModalLabel"
+    {{-- <div class="modal fade" id="editShiftModal" tabindex="-1" role="dialog" aria-labelledby="editShiftModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -566,7 +587,75 @@
                 </form>
             </div>
         </div>
+    </div> --}}
+
+    {{-- MODEL FOR ADD BILL IMAGE --}}
+
+    <!-- Modal -->
+    <div class="modal fade" id="billInfoModal" tabindex="-1" aria-labelledby="billInfoModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="billInfoModalLabel">Add Bill Info</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('bill_images.store') }}" method="POST" enctype="multipart/form-data"
+                        class="row g-3">
+                        @csrf
+                        <div class="col-md-12">
+                            @foreach($staffs as $staff)
+                            @if(session('username') == $staff->username || session('adminusername') == $staff->username)
+                            <label for="staff_name" class="form-label">User: </label>
+                            <input type="text" class="form-control" id="staff_name" name="staff_name"
+                                value="{{ $staff->staff_name }}" readonly>
+                            <input type="hidden" id="staff_id" name="staff_id" value="{{ $staff->id }}">
+                            @endif
+                            @endforeach
+                        </div>
+
+                        @if(session()->has('adminusername'))
+                        <div class="col-md-12">
+                            <label for="shop_id" class="form-label">Shop:</label>
+                            <select name="shop_id" id="shop_id" class="form-select">
+                                <option value="">Select a Shop</option>
+                                @foreach($shops as $shop)
+                                <option value="{{ $shop->id }}">{{ $shop->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        @else
+                        <div class="col-md-12">
+                            @foreach($staffs as $staff)
+                            @if(session('username') == $staff->username)
+                            <label for="shop_name" class="form-label">Shop: </label>
+                            <input type="text" class="form-control" id="shop_name" name="shop_name"
+                                value="{{ $staff->shop->name }}" readonly>
+                            <input type="hidden" id="shop_id" name="shop_id" value="{{ $staff->shop_id }}">
+                            @endif
+                            @endforeach
+                        </div>
+                        @endif
+                        <div class="col-md-12">
+                            <label for="date" class="form-label">Date:</label>
+                            <input type="date" class="form-control" id="date" name="date">
+                        </div>
+                        <div class="col-md-12">
+                            <label for="image" class="form-label">Image:</label>
+                            <input type="file" name="image" id="image" class="form-control" required>
+                        </div>
+                        <div class="col-md-12 mt-3">
+                            <button type="submit" class="btn btn-success rounded-pill w-100">
+                                <i class="fa-solid fa-floppy-disk me-1"></i> Upload
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
+
+    {{-- MODEL FOR ADD BILL IMAGE END --}}
 
 
     <script>
